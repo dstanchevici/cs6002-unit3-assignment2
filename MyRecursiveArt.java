b61ac8cb81ed6d9df7ae97e171b3475b2bb349e8
Dmitri Stanchevici <dmitristanchevici@Dmitris-iMac.local>
2023-02-15 08:33:55 -0500

    added recursion to drawCircle in Problem 4(ii)

--- MyRecursiveArt.java
+++ MyRecursiveArt.java
@@ -7,26 +7,40 @@ public class MyRecursiveArt {
 
 	drawPattern (1.0, 9.0);
 
-	drawCircle (7.0, 5.0);
+	drawCircle (5.5, 3.5, 2.0);
     }
 
-    static void drawCircle (double x, double y)
+    static void drawCircle (double x, double y, double radius)
     {
-	DrawTool.drawCircle (x,y, 2.0);
-	drawSegments (x, y, 2.0, 10.0);
+	// Base
+	if (radius < 0.45) {
+	    return;
+	}
+
+	// Draw
+	DrawTool.drawCircle (x,y, radius);
+	drawSegments (x, y, radius, 10.0);
+
+	// Recurse
+	x += 1.1;
+	y += 1.5;
+	radius -= 0.4;
+	drawCircle (x,y, radius);
     }
 
     static void drawSegments (double x, double y, double radius, double angle)
     {
-
+	// Base
 	if (angle >= 60) {
 	    return;
 	}
 
+	// Draw 
 	double newY = y + (Math.sin(angle) * radius);
 	double newX  = x - (Math.cos(angle) * radius);
 	DrawTool.drawLine (x,y, newX,newY);
 
+	// Recurse
 	angle += 10;
 	drawSegments (x, y, radius, angle);
 
